f2c348673ae0dc84774e61a226fdae7f359b7fa0
Nicolas San Pedro <nicolas.sanpedro@rappi.com>
2021-03-11 20:53:36 -0300

    added pagination strcture

--- src/pages/Planets/PlanetsPage.jsx
+++ src/pages/Planets/PlanetsPage.jsx
@@ -1,20 +1,27 @@
-import React, { useEffect } from 'react';
-import CheckboxComponent from '../../components/Checkbox/Checkbox';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
+import { Button, Badge } from 'shards-react';
+import CheckboxComponent from '../../components/Checkbox/Checkbox';
 import Card from '../../components/Card/Card';
 import PlanetsService from '../../services/PlanetsService';
 import Spinner from '../../components/Spinner/Spinner';
 
 const PlanetsPage = () => {
+  const [pageSelected, setPageSelected] = useState(1);
+
   const getPlanets = async () => {
-    const res = await PlanetsService.getPlanets();
+    const res = await PlanetsService.getPlanets(pageSelected);
 
     return res.json();
   };
 
-  const { data, status } = useQuery('planets', getPlanets);
+  const { data, status } = useQuery(['planets', pageSelected], getPlanets, {
+    keepPreviousData: true
+  });
 
-  // console.log('data ==> ', data);
+  useEffect(() => {
+    getPlanets();
+  }, []);
 
   return (
     <>
@@ -29,13 +36,41 @@ const PlanetsPage = () => {
             <div className='py-4'>
               {status === 'loading' && <Spinner />}
               {status === 'error' && <div>ERROR</div>}
-              {status === 'success' &&
-                data &&
-                data.results.map((planet) => (
+              {status === 'success' && (
+                <>
+                  <Button
+                    outline
+                    pill
+                    size='sm'
+                    onClick={() =>
+                      setPageSelected((old) => Math.max(old - 1, 1))
+                    }
+                    disabled={pageSelected === 1}
+                  >
+                    Previous
+                  </Button>
+                  <Badge outline pill theme='light'>
+                    {pageSelected}
+                  </Badge>
+                  <Button
+                    outline
+                    pill
+                    size='sm'
+                    onClick={() => setPageSelected((old) => Math.max(old + 1))}
+                    disabled={!data.next}
+                  >
+                    Next
+                  </Button>
                   <div className='py-2'>
-                    <Card data={planet} />
+                    {data &&
+                      data.results.map((planet) => (
+                        <div className='py-2'>
+                          <Card data={planet} />
+                        </div>
+                      ))}
                   </div>
-                ))}
+                </>
+              )}
             </div>
           </div>
         </div>
--- src/services/PlanetsService.js
+++ src/services/PlanetsService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { PLANETS_URL } from '../config/config';
 
 class PlanetsService {
-  static getPlanets() {
-    return fetch(PLANETS_URL);
+  static getPlanets(page) {
+    return fetch(`http://swapi.dev/api/planets/?page=${page}`);
   }
 }
 
